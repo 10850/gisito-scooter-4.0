53fbacc4106aa51123c31ec74fa047a5105e9fc9
10850 <10850@student.eadania.dk>
2021-04-02 16:45:51 +0200

    almost there
    problems with usestorage need to be looked at

--- src/components/forms/AddDeck.js
+++ src/components/forms/AddDeck.js
@@ -1,119 +1,170 @@
 import { useState } from "react";
+import { db } from "../../firebase/firebase";
+import useStorage from "../../firebase/useStorage";
 import "../forms/addproduct.css";
 
 const AddDeck = () => {
-    /* const [deckData, setDeckData] = useState([])
+    const [deckData, setDeckData] = useState([])
     const [file, setFile] = useState([])
+    const [error, setError] = useState(null)
+    const { url } = useStorage(file);
+    
 
-    setDeckData({[e.target.name]: e.target.value})
-    setFile({[e.target.name]: e.target.files}) */
+    const dataChange = (e) => {
+        if (e.target.type === "number") {
+            console.log("number")
+            setDeckData({
+              [e.target.name]: parseInt(e.target.value)
+            }) 
+        } else {
+            setDeckData({...deckData, [e.target.name]: e.target.value})
+        }
+    }
+
+    const brandChange = async (e) => {
+        let selectedBrand = e.target.files[0];
+        let name = e.target.name;
+
+        if (selectedBrand) {
+          await setFile({...file, [name]: selectedBrand});
+          setFile({...file, [name]: url})
+          console.log(file)
+          setError('');
+        } 
+        
+    };
+
+    const productChange = async (e) => {
+        let selectedProduct = e.target.files[0];
+        let name = e.target.name;
+
+        if (selectedProduct) {
+          await setFile({...file, [name]: selectedProduct});
+          setFile({...file, [name]: url})
+          console.log(file)
+          setError('');
+        }
+    };
+
+
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(file)
+        
+        db.collection("decks").add({
+            deckData,
+            file
+        })
+    }
+
 
     return (
         <>
             <h3>
                 Tilføj Deck
             </h3>
-            <form className="product-form">
+            <form className="product-form" onSubmit={handleSubmit}>
                 <label>
                     Kategori
                 </label>
-                <input type="text" placeholder="bars" />
+                <input name="category" type="text" placeholder="bars" onChange={dataChange} />
 
                 <label>
                     Brand logo
                 </label>
-                <input type="file" />
+                <input name="brand" type="file" onChange={brandChange} />
 
                 <label>
                     Produktnavn
                 </label>
-                <input type="text" />
+                <input name="product_name" type="text" onChange={dataChange} />
 
                 <label>
                     Pris
                 </label>
-                <input type="number" step="any" />
+                <input name="price" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Produktbilled
                 </label>
-                <input type="file" />
+                <input name="product_img" type="file" onChange={productChange} />
 
                 <label>
                     Beskrivelse
                 </label>
-                <textarea className="description">
+                <textarea name="description" className="description" onChange={dataChange}>
 
                 </textarea>
 
                 <label>
                     Tech features
                 </label>
-                <textarea className="tech-feature">
+                <textarea name="tech_feature" className="tech-feature" onChange={dataChange}>
                     
                 </textarea>
 
                 <label>
                     Deck bredde
                 </label>
-                <input type="number" step="any" />
+                <input name="deck_width" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Deck Længde
                 </label>
-                <input type="number" step="any" />
+                <input name="deck_width" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Hjuldiameter
                 </label>
-                <input type="number" step="any" />
+                <input name="wheel_diameter" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Hjul nav bredde
                 </label>
-                <input type="number" step="any" />
+                <input name="wheel_nav_width" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Vægt
                 </label>
-                <input type="number" step="any" />
+                <input name="weight" type="number" step="any" onChange={dataChange} />
                     
                 <label>
                     Headtube vinkel
                 </label>
-                <input type="number" step="any" />
+                <input name="headtube_angle" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Headtube længde
                 </label>
-                <input type="number" step="any" />
+                <input name="headtube_length" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Deck spacers
                 </label>
-                <input type="text" />
+                <input name="deck_spacers" type="text" onChange={dataChange} />
 
                 <label>
                     Bremse/Fender
                 </label>
-                <input type="text" />
+                <input name="brake" type="text" onChange={dataChange} />
 
                 <label>
                     Bremse monteringbolt
                 </label>
-                <input type="text" />
+                <input name="brake_bolt" type="text" onChange={dataChange} />
 
                 <label>
                     Aksel diameter
                 </label>
-                <input type="number" step="any" />
+                <input name="aksel" type="number" step="any" onChange={dataChange} />
 
                 <label>
                     Griptape
                 </label>
-                <input type="text" />
+                <input name="griptape" type="text" onChange={dataChange} />
 
-                <button>
+                <button type="submit">
                     Tilføj
                 </button>
             </form>
--- src/firebase/useStorage.js
+++ src/firebase/useStorage.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react"
 import { productStorage } from "./firebase";
+import 'firebase/storage';
+import firebase from "firebase/app";
 
 const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
     const [error, setError] = useState(null);
-    const [url, setUrl] = useState([]);
+    const [url, setUrl] = useState(null);
 
-    useEffect(() => {
-        const storageRef = productStorage.ref(file.name);
-
-        storageRef.put(file).on('state_changed', (snap) => {
-            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-            setProgress(percentage);
+    useEffect(async () => {
+        // references
+        const storageRef = firebase.storage().ref("products/" + file.name);
+        
+        await storageRef.put(file).on('state_changed', (snapshot) => {
+          let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(percentage);
         }, (err) => {
-            setError(err);
+          setError(err);
         }, async () => {
-            const url = await storageRef.getDownloadURL();
-            setUrl(url);
-        })
-    }, [file]);
+          const url = await storageRef.getDownloadURL();
+          setUrl(url);
+        });
+      }, [file]);
 
     return { progress, url, error }
 }
